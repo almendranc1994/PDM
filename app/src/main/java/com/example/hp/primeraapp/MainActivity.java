314e64ec8bc6f398165d51ebb7d8c6f3c4c0fcfe
almendranc1994 <almendra.nunezc@pucp.pe>
2018-05-11 09:44:50 -0500

    Login

---
+++ app/src/main/java/com/example/hp/primeraapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.hp.primeraapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.bntLogin);
+        Info.setText("No of attempts remaining: 5");
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            //Asi me muevo a la siguiente actividad
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
+
+            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+}
